0f38bfc7fb9f932e3a37ba7469764276e3e83e6e
Uwooky <150152179+Uwooky@users.noreply.github.com>
2025-04-08 18:13:03 +0900

    접시 BP 생성

---
+++ Source/OverCooked/Public/LYW/FoodChecker.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FoodChecker.generated.h"
+
+UCLASS()
+class OVERCOOKED_API AFoodChecker : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFoodChecker();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UBoxComponent* BoxComp;
+
+	TArray<FString> IngredientArr = { TEXT("SeaWeed"), TEXT("Rice"), TEXT("Cucumber"), TEXT("Salmon") };
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class ADishActor* DishActor;
+
+	void PrintIngredient(ADishActor* dish);
+
+	UFUNCTION()
+	void BeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+};
