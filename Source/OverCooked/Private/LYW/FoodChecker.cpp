4e26b3d811305eb596f330bf4c720d5a014cfb6b
Uwooky <150152179+Uwooky@users.noreply.github.com>
2025-04-10 17:03:02 +0900

    trouble shooting2

--- Source/OverCooked/Private/LYW/FoodChecker.cpp
+++ Source/OverCooked/Private/LYW/FoodChecker.cpp
@@ -3,14 +3,16 @@
 
 #include "LYW/FoodChecker.h"
 #include "../../Public/LYW/DishActor.h"
+#include "Components/BoxComponent.h"
 
 
 // Sets default values
 AFoodChecker::AFoodChecker()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	//BoxComp->OnComponentEndOverlap
 	BoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComp"));
 	SetRootComponent(BoxComp);
 	BoxComp->OnComponentBeginOverlap.AddDynamic(this, &AFoodChecker::BeginOverlap);
@@ -21,30 +23,51 @@ void AFoodChecker::BeginPlay()
 {
 	Super::BeginPlay();
 
-	
+	for (int32 i = 0; i < 2; i++)
+	{
+		MakeRandomOrder();
+	}
+	currentTime = 0.0f;
 }
 
 // Called every frame
 void AFoodChecker::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	currentTime += DeltaTime;
 
+	if (currentTime > newOrderTime && CurrentOrder.Num() < 5)
+	{
+		MakeRandomOrder();
+	}
 }
 
 void AFoodChecker::PrintIngredient(ADishActor* dish)
 {
-	for (const FString& ing_str : IngredientArr)
+	FString Key;
+	Key = dish->Key;
+
+
+	for (int32 i = 0; i < CurrentOrder.Num(); i++)
 	{
-		bool* isHave = dish->Ingredient.Find(ing_str);
-
-		FString ingredientName = ing_str;
-		FString haveText = (isHave && *isHave) ? TEXT("true") : TEXT("false");
-
-		UE_LOG(LogTemp, Warning, TEXT("%s : %s"), *ingredientName, *haveText);
+		if (Key == CurrentOrder[i])
+		{
+			CurrentOrder.RemoveAt(i);
+			MakeRandomOrder();
+			UE_LOG(LogTemp, Warning, TEXT("%s"), *Key);
+			break;
+		}
 	}
+
+	// 잘못 제출 -> score 깎기???
 }
 
-
+void AFoodChecker::MakeRandomOrder()
+{
+	int32 menu = FMath::RandRange(0, 3);
+	CurrentOrder.Add(OrderInfo[menu]);
+	currentTime = 0.0f;
+}
 
 void AFoodChecker::BeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
